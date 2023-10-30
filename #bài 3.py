d1da0b10afb9a2895eb891350e9ea743ce0f24a5
TADUCHUNGDHKL17A1HN <149364006+TADUCHUNGDHKL17A1HN@users.noreply.github.com>
2023-10-30 15:38:36 +0700

    Add files via upload

---
+++ #bài 3.py
@@ -0,0 +1,9 @@
+#bài 3
+ten_hang="hộp sữa Vina Milk"
+so_luong=5
+don_gia=25000
+tien_phai_tra=so_luong*don_gia
+print("tên hàng",":",ten_hang)
+print("số lượng",":",so_luong)
+print("đơn giá",":",don_gia)
+print("tiền phải trả",":",tien_phai_tra)
