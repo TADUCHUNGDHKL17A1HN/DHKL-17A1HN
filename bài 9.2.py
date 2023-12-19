6d706eb2f04c7d469ddd49eecdf70eab120dbd83
TADUCHUNGDHKL17A1HN <149364006+TADUCHUNGDHKL17A1HN@users.noreply.github.com>
2023-12-19 16:08:56 +0700

    Add files via upload

---
+++ bài 9.2.py
@@ -0,0 +1,17 @@
+# bài 9.1
+def tinh_nam_am_lich(nam_duong_lich):
+    can = ["Quý", "Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ", "Canh", "Tân", "Nhâm"]
+    chi = ["Hợi", "Tí", "Sửu", "Dần", "Mão", "Thìn", "Tị", "Ngọ", "Mùi", "Thân", "Dậu", "Tuất"]
+
+    can_index = (nam_duong_lich - 4) % 10
+    chi_index = (nam_duong_lich - 4) % 12
+
+    nam_am_lich = "{} {}".format(can[can_index], chi[chi_index])
+
+    return nam_am_lich
+
+# Ví dụ sử dụng hàm
+nam_duong_lich = int(input("nhập năm :"))
+nam_am_lich = tinh_nam_am_lich(nam_duong_lich)
+
+print("Năm âm lịch tương ứng với năm {} là: {}".format(nam_duong_lich, nam_am_lich))
